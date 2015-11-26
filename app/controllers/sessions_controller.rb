5e7415fb6cf9dc2977d162c73ea007f7018876b4
Jennifer Tigner <jennifertigner@email.com>
2015-11-26 07:32:37 +0000

    Added pagination on admin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to movies_path, notice: "Adios!"
+    redirect_to movies_path, notice: "Goodbye!"
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password
 
+  paginates_per 10
+
   def full_name
     "#{firstname} #{lastname}"
   end
