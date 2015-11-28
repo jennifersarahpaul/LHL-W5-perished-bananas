92b71b82bb2b88f0d2fd0446eca5ef9da169eea4
Jennifer Tigner <jennifertigner@email.com>
2015-11-28 00:49:25 +0000

    Changed resources :sessions to become a singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   
   root to: 'movies#index'
 
